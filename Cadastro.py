7381d870c758adde27d69f66e1de47a7fea798d8
Your Name <pedrovini20001@gmail.com>
2021-12-15 01:13:03 -0300

    CadastroQT

--- Cadastro.py
+++ Cadastro.py
@@ -5,11 +5,28 @@ from tkinter.constants import END
 from PyQt5 import uic, QtWidgets ### Solicita a abertura do QT designer 
 
 ###Criando Banco de Dados 
+def login():
+    tela.label.setText("")
+    usuario = tela_03.lineEdit.text()
+    senha = tela_03.lineEdit_2.text()
+    
+    if usuario == "Pedro2021" and senha == "123456" :     ### Campor editavel 
+        tela_03.close()
+        tela.show()
 
+    else:
+        tela_03.label.setText("Dados incorretos!")
+def sair():
+    tela.close() 
+    
+    tela_03.show()
+def sair_login():
+    tela_03.close()
 def salvar_dados():
 
     nome_cliente = tela.lineEdit_2.text()
     telefone = tela.lineEdit_3.text()
+    Cpf = tela.lineEdit_5.text()
     Email = tela.lineEdit_4.text()
     Cidade= tela.lineEdit_9.text()
     
@@ -24,13 +41,14 @@ def salvar_dados():
                 cod INTEGER PRIMARY KEY,
                 nome_cliente CHAR(40) NOT NULL,
                 telefone INTEGER(20),
+                Cpf INTEGER(11),
                 Email CHAR(40),
-                Cidade CHAR(40)
+                Cidade CHAR(50)
                             
              );
         """)
-        cursor.execute(""" INSERT INTO clientes(nome_cliente, telefone, Email, Cidade)
-         VALUES(?, ?, ?, ?)""", (nome_cliente, telefone , Email, Cidade))
+        cursor.execute(""" INSERT INTO clientes(nome_cliente, telefone, Cpf, Email, Cidade)
+         VALUES(?, ?, ?, ?, ?)""", (nome_cliente, telefone , Cpf, Email, Cidade, ))
 
         conexao.commit(); print("Banco de dados Criado.")
         cursor.close()
@@ -38,7 +56,6 @@ def salvar_dados():
 
     except sqlite3.Error as erro:
         print("Erro ao inserir os dados", erro)
-
 def listar_dados():
     tela_2.show()
     conexao = sqlite3.connect('Banco_cliente.db')
@@ -46,26 +63,29 @@ def listar_dados():
     cursor.execute("SELECT * FROM clientes")
     dados_lidos = cursor.fetchall()
     tela_2.tableWidget.setRowCount(len(dados_lidos))
-    tela_2.tableWidget.setColumnCount(5)
+    tela_2.tableWidget.setColumnCount(6)
 
     for i in range(0, len(dados_lidos)):
-        for k in range(0,5):
-           tela_2.tableWidget.setItem(i,k,QtWidgets.QTableWidgetItem(str(dados_lidos[i][k])))
+        for l in range(0,6):
+           tela_2.tableWidget.setItem(i,l,QtWidgets.QTableWidgetItem(str(dados_lidos[i][l])))
     
     
     conexao.close()
-
-
-         
-
+    
 
 ### Chama a aplicação de QTDesigner  
 
 app=QtWidgets.QApplication([])
+tela_03=uic.loadUi("tela_03.ui")### Chama a tela da aplicação feita no QTDsigner
 tela=uic.loadUi("Tela.ui") ### Chama a tela da aplicação feita no QTDsigner
-tela_2=uic.loadUi("tela_2.ui")
+tela_2=uic.loadUi("tela_2.ui")### Chama a tela da aplicação feita no QTDsigner
+tela_03.pushButton.clicked.connect(login)
 tela.bt_novo.clicked.connect(salvar_dados)
 tela.bt_buscar.clicked.connect(listar_dados)
 
-tela.show()
+tela.bt_sair.clicked.connect(sair)
+tela_03.bt_sairtela03.clicked.connect(sair_login)
+
+tela_03.show()
+
 app.exec()
